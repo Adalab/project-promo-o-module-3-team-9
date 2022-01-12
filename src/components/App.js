7fcf97d5238a5ca8276e336aee2c84499098d604
mpenacasares <m.penacasares@gmail.com>
2022-01-12 12:46:23 +0100

    add Length

--- src/components/App.js
+++ src/components/App.js
@@ -1,40 +1,70 @@
-import "../styles/App.css";
+import "../styles/App.scss";
 
 function App() {
   return (
     <div className="App">
       <header className="header">
         <div className="header__wrapper">
-          <a className="header__link" href="./" title="Team Undefined"><img className="header__img" src="./assets/images/logo-team-undefined.svg" alt="Team Undefined"/></a>
+          <a className="header__link" href="./" title="Team Undefined">
+            <img
+              className="header__img"
+              src="./assets/images/logo-team-undefined.svg"
+              alt="Team Undefined"
+            />
+          </a>
         </div>
       </header>
       <section className="profile-cards">
         <form className="form" action="">
           <section className="cards">
             <div className="cards__container">
-              <button className="cards__button--reset js-resetBtn" type="reset"><i className="far fa-trash-alt"></i>Reset</button>
+              <button className="cards__button--reset js-resetBtn" type="reset">
+                <i className="far fa-trash-alt"></i>Reset
+              </button>
               <article className="cards__article">
                 <div className="js-borderPalette cards__wrapper--palette1">
-                  <h1 className="cards__title js-cardName cards__title--palette1">Nombre Apellido</h1>
-                  <h2 className="cards__subtitle js-cardOccupation">Front-end developer</h2>
+                  <h1 className="cards__title js-cardName cards__title--palette1">
+                    Nombre Apellido
+                  </h1>
+                  <h2 className="cards__subtitle js-cardOccupation">
+                    Front-end developer
+                  </h2>
                 </div>
                 <div className="cards__image js__profile-image"></div>
                 <ul className="cards__contact">
                   <li className="cards__item">
-                    <a className="cards__link cards__link--phone js-phonePreview js-linksIcons cards__link--palette1"
-                      title="Teléfono"><i className="fas fa-mobile-alt"></i></a>
+                    <a
+                      className="cards__link cards__link--phone js-phonePreview js-linksIcons cards__link--palette1"
+                      title="Teléfono"
+                    >
+                      <i className="fas fa-mobile-alt"></i>
+                    </a>
                   </li>
                   <li className="cards__item">
-                    <a className="cards__link cards__link--email js-previewEmail js-linksIcons cards__link--palette1"
-                      title="Email"><i className="far fa-envelope"></i></a>
+                    <a
+                      className="cards__link cards__link--email js-previewEmail js-linksIcons cards__link--palette1"
+                      title="Email"
+                    >
+                      <i className="far fa-envelope"></i>
+                    </a>
                   </li>
                   <li className="cards__item">
-                    <a className="cards__link cards__link--linkedin js-cardLinkedin js-linksIcons cards__link--palette1"
-                      title="LinkedIn" target="_blank"><i className="fab fa-linkedin-in"></i></a>
+                    <a
+                      className="cards__link cards__link--linkedin js-cardLinkedin js-linksIcons cards__link--palette1"
+                      title="LinkedIn"
+                      target="_blank"
+                    >
+                      <i className="fab fa-linkedin-in"></i>
+                    </a>
                   </li>
                   <li className="cards__item">
-                    <a className="cards__link cards__link--github js-cardGitHub js-linksIcons cards__link--palette1"
-                      title="GitHub" target="_blank"><i className="fab fa-github-alt"></i></a>
+                    <a
+                      className="cards__link cards__link--github js-cardGitHub js-linksIcons cards__link--palette1"
+                      title="GitHub"
+                      target="_blank"
+                    >
+                      <i className="fab fa-github-alt"></i>
+                    </a>
                   </li>
                 </ul>
               </article>
@@ -60,7 +90,7 @@ function App() {
                       id="palette1"
                       name="palette"
                       className="js-labelPalette design__input"
-                      checked
+                      defaultChecked
                     />
 
                     <label htmlFor="palette1" className="design__label">
@@ -118,35 +148,91 @@ function App() {
               </legend>
 
               <section className="box__main fill__section js-sectionFill hidden">
-                <label className="fill__label" htmlFor="name">Nombre completo</label>
-                <input className="fill__input js-name" type="text" name="name" id="name" placeholder="Ej: Sally Jill"
-                  maxlength="16" />
+                <label className="fill__label" htmlFor="name">
+                  Nombre completo
+                </label>
+                <input
+                  className="fill__input js-name"
+                  type="text"
+                  name="name"
+                  id="name"
+                  placeholder="Ej: Sally Jill"
+                  maxLength="16"
+                />
 
-                <label className="fill__label" htmlFor="job">Puesto</label>
-                <input className="fill__input js-occupation" type="text" name="job" id="job" placeholder="Ej: Front-end unicorn"
-                  maxlength="24" />
+                <label className="fill__label" htmlFor="job">
+                  Puesto
+                </label>
+                <input
+                  className="fill__input js-occupation"
+                  type="text"
+                  name="job"
+                  id="job"
+                  placeholder="Ej: Front-end unicorn"
+                  maxLength="24"
+                />
 
                 <label className="fill__label">Imagen de perfil</label>
                 <div className="fill__container">
-                  <label className="fill__button js__profile-trigger" htmlFor="img-selector" type="button">Añadir imagen</label>
-                  <input type="file" name="img-selector" id="img-selector" className="js__profile-upload-btn hidden"/>
-                    <div className="fill__image js__profile-preview"></div>
+                  <label
+                    className="fill__button js__profile-trigger"
+                    htmlFor="img-selector"
+                    type="button"
+                  >
+                    Añadir imagen
+                  </label>
+                  <input
+                    type="file"
+                    name="img-selector"
+                    id="img-selector"
+                    className="js__profile-upload-btn hidden"
+                  />
+                  <div className="fill__image js__profile-preview"></div>
                 </div>
 
-                <label className="fill__label" htmlFor="phone">Teléfono</label>
-                <input className="js-fill_phone fill__input" type="tel" name="phone" id="phone" placeholder="Ej: 555-55-55-55" />
+                <label className="fill__label" htmlFor="phone">
+                  Teléfono
+                </label>
+                <input
+                  className="js-fill_phone fill__input"
+                  type="tel"
+                  name="phone"
+                  id="phone"
+                  placeholder="Ej: 555-55-55-55"
+                />
 
-                <label className="fill__label" htmlFor="email">Email</label>
-                <input className="fill__input js-inputEmail" type="email" name="email" id="email"
-                  placeholder="Ej: sally-hill@gmail.com" />
+                <label className="fill__label" htmlFor="email">
+                  Email
+                </label>
+                <input
+                  className="fill__input js-inputEmail"
+                  type="email"
+                  name="email"
+                  id="email"
+                  placeholder="Ej: sally-hill@gmail.com"
+                />
 
-                <label className="fill__label" htmlFor="linkedin">LinkedIn</label>
-                <input className="fill__input js-inputLinkedin" type="text" name="linkedin" id="linkedin"
-                  placeholder="Ej: linkedin.com/in/sally.hill" />
+                <label className="fill__label" htmlFor="linkedin">
+                  LinkedIn
+                </label>
+                <input
+                  className="fill__input js-inputLinkedin"
+                  type="text"
+                  name="linkedin"
+                  id="linkedin"
+                  placeholder="Ej: linkedin.com/in/sally.hill"
+                />
 
-                <label className="fill__label" htmlFor="github">GitHub</label>
-                <input className="fill__input js-inputGitHub" type="text" name="github" id="github"
-                  placeholder="Ej: github.com/sally-hill" />
+                <label className="fill__label" htmlFor="github">
+                  GitHub
+                </label>
+                <input
+                  className="fill__input js-inputGitHub"
+                  type="text"
+                  name="github"
+                  id="github"
+                  placeholder="Ej: github.com/sally-hill"
+                />
               </section>
             </fieldset>
             <fieldset className="box share">
@@ -171,27 +257,43 @@ function App() {
 
                 <div className="js-shareDone share__done hidden">
                   <p className="share__text">La tarjeta ha sido creada:</p>
-                  <a className="js-shareLink share__link" href="#" target="_blank"
-                  >Your URL will be placed here!</a
+                  <a
+                    className="js-shareLink share__link"
+                    href="#"
+                    target="_blank"
                   >
-                  <a className="share__button share__button--twitter js-shareTwitter"
-                  ><i className="fab fa-twitter"></i>Compartir en twitter</a
-                  >
+                    Your URL will be placed here!
+                  </a>
+                  <a className="share__button share__button--twitter js-shareTwitter">
+                    <i className="fab fa-twitter"></i>Compartir en twitter
+                  </a>
                 </div>
 
                 <div className="js-shareError share__done hidden">
-                  <p className="share__error">ERROR, te faltan campos por rellenar</p>
+                  <p className="share__error">
+                    ERROR, te faltan campos por rellenar
+                  </p>
                 </div>
               </section>
             </fieldset>
-
           </div>
         </form>
       </section>
       <footer className="footer">
         <div className="footer__wrapper">
           <p className="footer__text">Team Undefined &copy; 2021</p>
-          <a className="footer__link" href="https://adalab.es/" target="_blank" title="Adalab"><img className="footer__img" src="./assets/images/logo-adalab.png" alt="Logo Adalab"/></a>
+          <a
+            className="footer__link"
+            href="https://adalab.es/"
+            target="_blank"
+            title="Adalab"
+          >
+            <img
+              className="footer__img"
+              src="./assets/images/logo-adalab.png"
+              alt="Logo Adalab"
+            />
+          </a>
         </div>
       </footer>
     </div>
